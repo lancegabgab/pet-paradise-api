95d89a0f6fe22737246612d1302e6cce7add3921
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-02-26 13:52:11 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
-const secret = "michaelgab";
+const secret = process.env.JWT_SECRET;
 
 module.exports.createAccessToken = (user) => {
 
