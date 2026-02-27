2cacbc75fa168c602f73d8e9f91a7650cda5d50c
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-02-27 08:39:07 +0800

    Updated auth.js
    Refactored code

--- auth.js
+++ auth.js
@@ -3,77 +3,46 @@ require('dotenv').config();
 
 const secret = process.env.JWT_SECRET;
 
-module.exports.createAccessToken = (user) => {
-
-	//payload
+const createAccessToken = (user) => {
 	const data = {
 		id: user._id,
 		email: user.email,
 		isAdmin: user.isAdmin
 	};
-
 	return jwt.sign(data, secret, {});
 }
 
-module.exports.verify = (req, res, next) => {
-    console.log(req.headers.authorization);
-
-    // "req.headers.authorization" contains sensitive data and especially our token
+const verify = (req, res, next) => {
     let token = req.headers.authorization;
 
-    // This if statement will first check if a token variable contains "undefined" or a proper jwt.  we will check token's data type with "typeof", if it is "undefined" we will send a message to the client. Else if it is not, then we return the token.
-    //walang binigay na token
     if(typeof token === "undefined"){
         return res.send({ auth: "Failed. No Token" });
     } else {
-    //kung hindi undefined, isslice yung token para hindi mainclude yung bearer
-        console.log(token);		
+    //slice yung token para hindi mainclude yung bearer	
         token = token.slice(7, token.length);
-        console.log(token);
-
-        //icocompare si token saka secret key
-        //pag di nagmatch error sya
-        //pag nagmatch papakita decoded token
         jwt.verify(token, secret, function(err, decodedToken){
-            
-            //If there was an error in verification, an erratic token, a wrong secret within the token, we will send a message to the client.
             if(err){
                 return res.send({
                     auth: "Failed",
                     message: err.message
                 });
-
             } else {
-
-            	// Contains the data from our token
-            	console.log("result from verify method:")
-                console.log(decodedToken);
-                
-                // Else, if our token is verified to be correct, then we will update the request and add the user's decoded details.
                 req.user = decodedToken;
-
-                // next() is an expressJS function which allows us to move to the next function in the route. It also passes details of the request and response to the next function/middleware.
                 next();
             }
         })
     }
 };
 
-//Pang verify if the user is an admin
-module.exports.verifyAdmin = (req, res, next) => {
-//ichecheck if si user is admin
-	//paghindi admin -- action forbidden
-	//pagadmin sige next go proceed gamitin mo yung feature
-	
+const verifyAdmin = (req, res, next) => {
 	if(req.user.isAdmin){
-		
 		next();
 	} else {
-		// Else, end the request-response cycle by sending the appropriate response and status code.
 		return res.status(403).send({
 			auth: "Failed",
 			message: "Action Forbidden"
 		})
 	}
-
 }
+
+module.exports = { createAccessToken, verify, verifyAdmin };
