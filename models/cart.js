b0197683d2b9b37017ca0736a5e43624c4914a78
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-02 11:02:48 +0800

    Updated cart model
    Referenced Product

--- models/cart.js
+++ models/cart.js
@@ -5,37 +5,32 @@ const cartItemSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-
   items: [ 
     {
       productId: {
-        type: String,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
         required: true,
       },
-
       quantity: {
         type: Number,
         required: true,
-        min: 1, // Ensure the quantity is at least 1
+        min: 1, 
       },
-
       price: {
-        type: Number, // Assuming the price is a number, adjust accordingly if needed
+        type: Number, 
         required: false,
       },
-
       subtotal: {
         type: Number,
-        required: false, //naka false muna para di nya i require
+        required: false, 
       },
     }
   ],
-
   total: {
     type: Number,
     required: true,
   },
-
   orderedOn: {
     type: Date,
     default: Date.now,
