cebf901059a9df10e1e0d228720eb458aa536a90
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-06 23:05:59 +0800

    Update users.js
    cleaned user routes

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/users");
-
 const auth = require("../auth");
-//Deconstructed the auth to directly access verify function and verifyAdmin
-const {verify, verifyAdmin} = auth;
 
+const { verify, verifyAdmin } = auth;
 
-// all the routes / requirements of controllers (services)
 router.post("/", userController.registerUser);
-
-//login for both users and admin
 router.post("/login", userController.loginUser);
-
-//retrieve all users
 router.get("/all", verify, verifyAdmin, userController.getAllUsers);
-
-//retrieve user details
 router.get('/details', verify, userController.getProfile);
-
-// update user as admin
 router.patch("/:userId/set-as-admin", verify, verifyAdmin, userController.updateAdmin);
-// reset password for all users
 router.patch('/update-password', verify, userController.resetPassword);
-
 
 module.exports = router;
 
