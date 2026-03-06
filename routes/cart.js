5130fed8bb6dbd67061e4bbca7d4660ff786fea0
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-06 23:07:42 +0800

    Update cart.js
    cleaned cart routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,25 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cart");
-
 const auth = require("../auth");
-const {verify, verifyAdmin} = auth;
 
-//S52 Endpoint requirements:
-//retrieve user carts
+const { verify, verifyAdmin } = auth;
+
 router.get('/get-cart', verify, cartController.getUserCart);
-//add to cart
 router.post('/add-to-cart', verify, cartController.addToCart);
-//update cart quantity
 router.patch('/update-cart-quantity', verify, cartController.changeQuantity);
-
-
-//S53 Endpoint requirements 
-//remove item from cart
 router.delete('/:productId/remove-from-cart', verify, cartController.removeProductFromCart);
-//clear cart
 router.delete('/clear-cart', verify, cartController.clearCartItems);
-
 
 module.exports = router;
 
