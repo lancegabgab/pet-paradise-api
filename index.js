858680a41a2a6bf4e87a49be611f82315807c492
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-23 20:35:43 +0800

    Updated index.js
    removed mongoose connection, used connectDB from mongoose.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require("express");
-const mongoose = require("mongoose");
 const userRoutes = require("./routes/users");
 const productRoutes = require("./routes/products");
 const cartRoutes = require("./routes/cart");
 const orderRoutes = require("./routes/order");
+const connectDB = require("./config/mongoose");
 
 const port = process.env.PORT;
 
@@ -16,12 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-mongoose.connect(process.env.CONN_STRING, 
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	});
-mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas'));
+connectDB();
 
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
