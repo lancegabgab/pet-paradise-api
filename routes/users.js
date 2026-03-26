df48e89af0b4b09c67d7f69f1085dffc8c8aed37
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-26 08:58:15 +0800

    Update user route
    adjusted import path of renamed user controller

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/users");
+const userController = require("../controllers/user.controller");
 const auth = require("../auth");
 
 const { verify, verifyAdmin } = auth;
