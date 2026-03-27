09d17e96deca8d17174ec1c9294929db2a278185
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-27 11:08:47 +0800

    Created cartItem.model.js

---
+++ models/cartItem.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const cartItemSchema = new mongoose.Schema({
+  cartId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Cart",
+    required: true,
+  },
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("CartItem", cartItemSchema);
