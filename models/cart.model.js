3d9c1e14c7b815a876f927357390620ec0bdbfa3
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-27 10:30:52 +0800

    Updated cart.model.js
    added timestamps

--- models/cart.model.js
+++ models/cart.model.js
@@ -27,6 +27,6 @@ const cartItemSchema = new mongoose.Schema({
     type: Number,
     required: true,
   }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Cart', cartItemSchema);
