8d6d653ea1d4f9eabc67ffbf6d817ce4a85cc854
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-27 08:32:21 +0800

    Updated cart.routes.js
    used verify in middlewares

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cart.controller");
-const auth = require("../auth");
-
-const { verify, verifyAdmin } = auth;
+const verify = require("../middlewares/verify");
 
 router.get('/get-cart', verify, cartController.getUserCart);
 router.post('/add-to-cart', verify, cartController.addToCart);
