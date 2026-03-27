404813a466297260a6dae2a363a9c96e137d02f2
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-27 10:33:06 +0800

    Update order model
    added timestamps

--- models/order.model.js
+++ models/order.model.js
@@ -30,12 +30,8 @@ const orderSchema = new mongoose.Schema({
   totalPrice: {
     type: Number,
     required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Order', orderSchema);
 
