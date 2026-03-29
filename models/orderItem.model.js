3237279fe77fec451353f8a69de11206dc2fa657
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-29 12:18:23 +0800

    Created orderItem schema

---
+++ models/orderItem.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const orderItemSchema = new mongoose.Schema({
+  orderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Order',
+    required: true,
+  },
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+  }
+}, { timestamps: true }); 
+
+module.exports = mongoose.model('OrderItem', orderItemSchema);
