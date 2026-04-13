eca2ac841fb3be479e84a41d8311134712dc90cc
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-04-13 14:09:12 +0800

    Updated Cart model
    referenced User, added unique: true

--- models/cart.model.js
+++ models/cart.model.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
   userId: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
+    unique: true,
   }
 }, { timestamps: true });
 
