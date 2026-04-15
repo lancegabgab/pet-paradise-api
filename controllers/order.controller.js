decf24e7f6b7894cc1142921fb518b37a77dea19
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-04-15 09:20:50 +0800

    Updated order.controller.js
    added message on response json

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -23,6 +23,7 @@ const getUserOrders = async (req, res) => {
     const orders = await orderService.getUserOrders(req.user?.id);
     res.status(200).json({
       success: true,
+      message: "Successfuly fetched orders",
       data: orders
     });
   } catch (error) {
@@ -39,6 +40,7 @@ const getAllOrders = async (req, res) => {
     const orders = await orderService.getAllOrders();
     res.status(200).json({
       success: true,
+      message: "Successfuly fetched orders",
       data: orders
     });
   } catch (error) {
